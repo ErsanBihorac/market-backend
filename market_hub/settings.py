7370f647dfb8d6e977ce18f2a7ffb4f29ef236ec
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2024-11-20 16:11:37 +0100

    created registration view and serializer

--- market_hub/settings.py
+++ market_hub/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'market_app',
     'user_auth_app',
 ]
@@ -131,6 +132,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
--- user_auth_app/api/urls.py
+++ user_auth_app/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import TestView
+from .views import UserProfileList, UserProfileDetail, RegistrationView
 
 urlpatterns = [
-    path('test/', TestView.as_view()),
+    path('profiles/', UserProfileList.as_view(), name='userprofile-list'),
+    path('profiles/<int:pk>/', UserProfileDetail.as_view(), name='userprofile-detail'),
+    path('registration/', RegistrationView.as_view(), name='registration'),
 ]
